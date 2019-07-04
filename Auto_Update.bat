ea848f6cf00edda5174bbea641f3431c1c048acd
fbodian <famarabodian@protonmail.com>
2019-07-04 10:30:02 +0000

    updated sysmon config path

--- Auto_Update.bat
+++ Auto_Update.bat
@@ -1,5 +1,5 @@
 @echo off
 cd C:\ProgramData\sysmon\
-@powershell Start-BitsTransfer -Source https://raw.githubusercontent.com/famsedition/Sysmon-Auto-Install/master/sysmonconfig-ionstorm.xml -Destination C:\ProgramData\sysmon\sysmonconfig-export.xml
-sysmon64 -c sysmonconfig-export.xml
+@powershell Start-BitsTransfer -Source https://raw.githubusercontent.com/famsedition/Sysmon-Auto-Install/master/sysmon-config.xml -Destination C:\ProgramData\sysmon\sysmon-config.xml
+sysmon64 -c sysmon-config.xml
 exit
--- Install_Sysmon.bat
+++ Install_Sysmon.bat
@@ -5,10 +5,10 @@ echo [+] Downloading Sysmon...
 @powershell Import-Module BitsTransfer
 @powershell Start-BitsTransfer -Source https://live.sysinternals.com/Sysmon64.exe -Destination C:\ProgramData\sysmon\sysmon64.exe
 echo [+] Downloading Sysmon files...
-@powershell Start-BitsTransfer -Source https://raw.githubusercontent.com/famsedition/Sysmon-Auto-Install/master/sysmonconfig-ionstorm.xml -Destination C:\ProgramData\sysmon\sysmonconfig-export.xml
+@powershell Start-BitsTransfer -Source https://raw.githubusercontent.com/famsedition/Sysmon-Auto-Install/master/sysmon-config.xml -Destination C:\ProgramData\sysmon\sysmonconfig-export.xml
 @powershell Start-BitsTransfer -Source https://raw.githubusercontent.com/famsedition/Sysmon-Auto-Install/master/Auto_Update.bat -Destination C:\ProgramData\sysmon\Auto_Update.bat
 @powershell Start-BitsTransfer -Source https://raw.githubusercontent.com/famsedition/Sysmon-Auto-Install/master/Uninstall.bat -Destination C:\ProgramData\sysmon\Uninstall.bat
-sysmon64.exe -accepteula -i sysmonconfig-export.xml
+sysmon64.exe -accepteula -i sysmon-config.xml
 echo [+] Sysmon Successfully Installed!
 echo [+] Creating Auto Update Task set to Hourly..
 SchTasks /Create /RU SYSTEM /RL HIGHEST /SC HOURLY /TN Update_Sysmon_Rules /TR C:\ProgramData\sysmon\Auto_Update.bat /F
