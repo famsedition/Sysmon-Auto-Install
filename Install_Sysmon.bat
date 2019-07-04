c09b068a286e57c7a68861d816dc028a4eb6d78f
fbodian <famarabodian@protonmail.com>
2019-07-04 16:51:28 +0000

    Update Install_Sysmon.bat

--- Install_Sysmon.bat
+++ Install_Sysmon.bat
@@ -1,6 +1,5 @@
 @echo off
 mkdir C:\ProgramData\sysmon
-pushd "C:\ProgramData\sysmon\"
 echo [+] Downloading Sysmon...
 @powershell Import-Module BitsTransfer
 @powershell Start-BitsTransfer -Source https://live.sysinternals.com/Sysmon64.exe -Destination C:\ProgramData\sysmon\sysmon64.exe
@@ -8,7 +7,7 @@ echo [+] Downloading Sysmon files...
 @powershell Start-BitsTransfer -Source https://raw.githubusercontent.com/famsedition/Sysmon-Auto-Install/master/sysmon-config.xml -Destination C:\ProgramData\sysmon\sysmon-config.xml
 @powershell Start-BitsTransfer -Source https://raw.githubusercontent.com/famsedition/Sysmon-Auto-Install/master/Auto_Update.bat -Destination C:\ProgramData\sysmon\Auto_Update.bat
 @powershell Start-BitsTransfer -Source https://raw.githubusercontent.com/famsedition/Sysmon-Auto-Install/master/Uninstall.bat -Destination C:\ProgramData\sysmon\Uninstall.bat
-sysmon64.exe -accepteula -i sysmon-config.xml
+C:\ProgramData\sysmon\sysmon64.exe -accepteula -i C:\ProgramData\sysmon\sysmon-config.xml
 echo [+] Sysmon Successfully Installed!
 echo [+] Creating Auto Update Task set to Hourly..
 SchTasks /Create /RU SYSTEM /RL HIGHEST /SC HOURLY /TN Update_Sysmon_Rules /TR C:\ProgramData\sysmon\Auto_Update.bat /F
