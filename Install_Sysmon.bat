2db80ee61226329bf9838020f8d39f403c2c19db
fbodian <famarabodian@protonmail.com>
2019-07-04 16:19:44 +0000

    minor change

--- Install_Sysmon.bat
+++ Install_Sysmon.bat
@@ -5,7 +5,7 @@ echo [+] Downloading Sysmon...
 @powershell Import-Module BitsTransfer
 @powershell Start-BitsTransfer -Source https://live.sysinternals.com/Sysmon64.exe -Destination C:\ProgramData\sysmon\sysmon64.exe
 echo [+] Downloading Sysmon files...
-@powershell Start-BitsTransfer -Source https://raw.githubusercontent.com/famsedition/Sysmon-Auto-Install/master/sysmon-config.xml -Destination C:\ProgramData\sysmon\sysmonconfig-export.xml
+@powershell Start-BitsTransfer -Source https://raw.githubusercontent.com/famsedition/Sysmon-Auto-Install/master/sysmon-config.xml -Destination C:\ProgramData\sysmon\sysmon-config.xml
 @powershell Start-BitsTransfer -Source https://raw.githubusercontent.com/famsedition/Sysmon-Auto-Install/master/Auto_Update.bat -Destination C:\ProgramData\sysmon\Auto_Update.bat
 @powershell Start-BitsTransfer -Source https://raw.githubusercontent.com/famsedition/Sysmon-Auto-Install/master/Uninstall.bat -Destination C:\ProgramData\sysmon\Uninstall.bat
 sysmon64.exe -accepteula -i sysmon-config.xml
